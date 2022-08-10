fcaf6c41409fc1e1cf9b3a49b37389e7de110898
drdray1 <draytonlav@gmail.com>
2022-08-10 03:02:38 -0700

    Initial Commit

---
+++ Raffle/RaffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaffleApp.swift
+//  Raffle
+//
+//  Created by Drayton Lavender on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RaffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
